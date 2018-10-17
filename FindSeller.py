0f988a9ae8ae4111d42e2dc1d26dbd6a1ef91a96
Andersound <fa8@kth.se>
2018-10-17 08:38:29 +0200

    En testverision av kod som läser in websida och hämtar alla säljare

--- FindSeller.py
+++ FindSeller.py
@@ -1,6 +1,7 @@
 
-import urllib.request
-import urllib.parse
+#from lxml import html
+import requests
+from bs4 import BeautifulSoup
 
 from bookClass import bookObject
 
@@ -15,4 +16,16 @@ def main():
     b1 = bookObject({'author': 'astrid','title':'pippi'})
     url = b1.getURL({'page':1, 'order':'asc', 'sortby':'price'}) #example url
     print(url)
+
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    sellers = soup.find_all(class_='seller')
+    for item in sellers:
+        seller = item.text.strip('Säljare:').strip()#strip initial 'Säljare:' and following whitespaces
+        seller = seller.strip('(företag)').strip()  #strip trailing '(företag)' and trailing whitespaces
+        print(seller)
+
+
 main()
+#Säljare:
+#                                Riddare
